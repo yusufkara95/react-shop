8fb779b28f05622a45b2ec0ff873313af3aacab4
Yusuf Kara <yusufkara@gmx.de>
2024-01-01 02:24:08 +0100

    Added the function to fetch the data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      products: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((products) =>
+        this.setState(
+          () => {
+            return { products: products };
+          },
+          () => {
+            console.log(this.state);
+            // Überprüfe, ob die API ein Array zurückgibt
+            //console.log(Array.isArray(products))
+          }
+        )
+      );
+  }
+
+  render() {
+    const filteredProducts = this.state.products.filter((product) => {
+      return product.title.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Produkt suchen"
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredProducts.map((product) => {
+          return (
+            <div key={product.id}>
+              <h2>{product.title}</h2>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
