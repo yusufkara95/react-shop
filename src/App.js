9f0bc20d70102fd6964e5ae2ae77cbbb9149b4c7
yusufkara95 <yusufkara@gmx.de>
2024-01-23 22:55:49 +0100

    Auslagerung des Inhaltes in eigenen Komponenten.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from 'react-router-dom'
 
-import "./categories.style.scss";
+import Navigation from './routes/navigation/navigation.component';
+import Home from "./routes/home/home.component";
+
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hüte",
-      imageUrl: "https://images.pexels.com/photos/4487515/pexels-photo-4487515.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 2,
-      title: "Jacken",
-      imageUrl: "https://images.pexels.com/photos/6220658/pexels-photo-6220658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 3,
-      title: "T-Shirt",
-      imageUrl: "https://images.pexels.com/photos/9491361/pexels-photo-9491361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-      id: 4,
-      title: "Damen",
-      imageUrl: "https://images.pexels.com/photos/18639550/pexels-photo-18639550/free-photo-of-mann-paar-frau-bett.jpeg",
-    },
-    {
-      id: 5,
-      title: "Herren",
-      imageUrl: "https://images.pexels.com/photos/18639550/pexels-photo-18639550/free-photo-of-mann-paar-frau-bett.jpeg",
-    },
-  ];
-
   return (
-    <Directory categories={categories} />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
