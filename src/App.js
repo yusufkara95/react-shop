651fa490eb4d1aebfcfd988bcb4d13415ce54847
Yusuf Kara <yusufkara@gmx.de>
2024-01-23 01:21:08 +0100

    Kategorie-Item und Directory wurde hinzugefügt.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from "./components/directory/directory.component";
 
-function App() {
+import "./categories.style.scss";
+
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hüte",
+      imageUrl: "https://images.pexels.com/photos/4487515/pexels-photo-4487515.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      title: "Jacken",
+      imageUrl: "https://images.pexels.com/photos/6220658/pexels-photo-6220658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 3,
+      title: "T-Shirt",
+      imageUrl: "https://images.pexels.com/photos/9491361/pexels-photo-9491361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 4,
+      title: "Damen",
+      imageUrl: "https://images.pexels.com/photos/18639550/pexels-photo-18639550/free-photo-of-mann-paar-frau-bett.jpeg",
+    },
+    {
+      id: 5,
+      title: "Herren",
+      imageUrl: "https://images.pexels.com/photos/18639550/pexels-photo-18639550/free-photo-of-mann-paar-frau-bett.jpeg",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Directory categories={categories} />
   );
-}
+};
 
 export default App;
