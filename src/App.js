baa84ee16e7ada5a13089aa0db7efe317904721c
yusufkara95 <yusufkara@gmx.de>
2024-01-27 19:14:21 +0100

    Authentication wurde hinzugefügt.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Navigation from './routes/navigation/navigation.component';
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
-
+import Authentication from "./routes/authentication/authentication.component";
 
 const Shop = () => {
-  return <h1>I am the shop page</h1>
+  return <h1>I am the shop page</h1>;
 };
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
