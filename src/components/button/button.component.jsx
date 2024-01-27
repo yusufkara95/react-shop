37dba16203f69f2d80a4e06047d2020c567d509a
yusufkara95 <yusufkara@gmx.de>
2024-01-27 19:14:38 +0100

    Button Komponent wurde hinzugefügt.

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./button.styles.scss";
+
+/* Variants of button: default, inverted, google-sign-in */
+const BUTTON_TYPE_CLASSES = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]} `}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
