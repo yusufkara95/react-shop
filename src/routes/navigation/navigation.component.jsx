8ba2e3ed25f4bdd10d532f63bb330b6e19a795d7
yusufkara95 <yusufkara@gmx.de>
2024-01-27 19:15:15 +0100

    Ein weiterer Link wurde hinzugefügt.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,26 +1,29 @@
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import logo from '../../assets/logo.png';
+import logo from "../../assets/logo.png";
 
-import './navigation.styles.scss';
+import "./navigation.styles.scss";
 
 const Navigation = () => {
-    return (
-        <Fragment>
-            <div className="navigation">
-                <Link className="logo-container" to='/'>
-                    <img width={64} src={logo} alt="Logo" className='logo' />
-                </Link>
-                <div className="nav-links-container">
-                    <Link className="nav-link" to='/shop'>
-                        SHOP
-                    </Link>
-                </div>
-            </div>
-            <Outlet />
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img width={64} src={logo} alt="Logo" className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/auth">
+            LOGIN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
 export default Navigation;
